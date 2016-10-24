32f5d3136e173205e1f6846eb563d78eeec6b095
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-10-23 19:34:14 -0700

    rendered list, appended list, submit and delete buttons.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 $(document).ready(function(event){
-  // callFunction(xxxxxx);
 
   var state = {
     items: []
   };
 
+  function deleteItem(state, item){
+    for (var i = 0; i < state.items.length; i++){
+      if (state.items[i] == item){
+        state.items.splice(i, 1);
+      };
+    };
+  };
   //add items to a hash with array items
   function addItem(state, item){
     state.items.push(item);
@@ -13,7 +19,16 @@ $(document).ready(function(event){
   //publish new item in DOM
   function renderList(state, element){
     var itemsHTML = state.items.map(function(item){
-      return "<li>" + item + "</li>";
+    return '<li><span class="shopping-item">' + item +
+           '</span><div class="shopping-item-controls">' +
+           '<button class="shopping-item-toggle">' +
+           '<span class="button-label">check</span>' +
+          '</button> ' +
+          '<button class="shopping-item-delete">'+
+          '<span class="button-label">delete</span>'+
+          '</button>'+
+          '</div>' +
+          '</li>'
     });
     element.append(itemsHTML);
   };
@@ -23,14 +38,17 @@ $(document).ready(function(event){
     addItem(state, $('#shopping-list-entry').val());
     renderList(state, $(".shopping-list"));
   });
-});
 
-//strike through shopping item. cant untoggle.
-$('.shopping-item-toggle').click(function(event){
-  $(this).closest("li").css("text-decoration", "line-through");
-});
+  //strike through shopping item. cant untoggle.
+  $(document).on('click','.shopping-item-toggle', function(event){
+    $(this).closest("li").find('.shopping-item').toggleClass('shopping-item__checked');
+  });
 
-//delete shopping item. WORKS
-$('.shopping-item-delete').click(function(event){
-  $(this).closest("li").remove();
+  //delete shopping item. WORKS
+  $(document).on('click','.shopping-item-delete', function(event){
+    var itemName = $(this).closest("li").find('shopping-item').val();
+    console.log(itemName);
+    deleteItem(state, itemName);
+    $(this).closest("li").remove();
+  });
 });
